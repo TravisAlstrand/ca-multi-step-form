cd545b7dedbfc27848cae52d9de47768a03c5e12
Travis Alstrand <travisalstrand@gmail.com>
2023-12-13 13:33:46 -0800

    good enough

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const card = document.querySelector("#card");
+const error = card.lastElementChild;
 const progress = document.querySelector("#progress");
 const form = document.querySelector("form");
 const labelInput = form.querySelector("#labelInput");
@@ -8,6 +9,8 @@ let step = 1;
 let nameInput = "";
 let emailInput = "";
 let phoneInput = "";
+
+labelInput.lastElementChild.focus();
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -23,30 +26,30 @@ form.addEventListener("submit", (e) => {
 
 function validateStepOne(input) {
   if (input) {
-    progress.style.width = "33%";
     step++;
     updateForm(step);
     nameInput = input;
+    handleError();
   } else {
     labelInput.lastElementChild.style.border = "1px solid red";
-    console.log("empty name");
+    handleError("Please provide a name");
   }
 }
 
 function validateStepTwo(input) {
   if (input) {
     if (/^[^@]+@[^@.]+\.[a-z]+$/i.test(input)) {
-      progress.style.width = "66%";
       step++;
       updateForm(step);
       emailInput = input;
+      handleError();
     } else {
       labelInput.lastElementChild.style.border = "1px solid red";
-      console.log("invalid email");
+      handleError("Invalid email address");
     }
   } else {
     labelInput.lastElementChild.style.border = "1px solid red";
-    console.log("empty email");
+    handleError("Please provide an email address");
   }
 }
 
@@ -56,16 +59,17 @@ function validateStepThree(input) {
       console.log("complete progress animation");
       progress.classList.add("completed");
       phoneInput = input;
+      handleError();
       setTimeout(() => {
         completeForm();
       }, 600);
     } else {
       labelInput.lastElementChild.style.border = "1px solid red";
-      console.log("invalid phone");
+      handleError("Please use (123) 456-7890 or 123-456-7890 formatting");
     }
   } else {
     labelInput.lastElementChild.style.border = "1px solid red";
-    console.log("empty phone");
+    handleError("Please provide a phone number");
   }
 }
 
@@ -75,19 +79,24 @@ function updateForm(step) {
     btnCont.innerHTML = `
       <button type="submit" class="whole-btn">Next</button>
     `;
+    updateProgress("0%");
   } else if (step === 2) {
     updateLabelHTML("email", "email", emailInput, "joe@example.com");
     btnCont.innerHTML = `
       <button type="button" class="half-btn prev-btn">Previous</button>
       <button type="submit" class="half-btn">Next</button>
     `;
+    updateProgress("33%");
   } else if (step === 3) {
     updateLabelHTML("tel", "phone", phoneInput, "123-123-1234");
     btnCont.innerHTML = `
       <button type="button" class="half-btn prev-btn">Previous</button>
       <button type="submit" class="half-btn submit-btn">Submit</button>
     `;
+    updateProgress("66%");
   }
+  labelInput.lastElementChild.focus();
+  listenForPrevBtn();
 }
 
 function updateLabelHTML(type, name, value, placeholder) {
@@ -99,10 +108,33 @@ function updateLabelHTML(type, name, value, placeholder) {
   `;
 }
 
+function listenForPrevBtn() {
+  const prevBtn = form.querySelector(".prev-btn");
+  if (prevBtn) {
+    prevBtn.addEventListener("click", () => {
+      step--;
+      updateForm(step);
+    });
+  }
+}
+
+function updateProgress(amt) {
+  progress.style.width = amt;
+}
+
+function handleError(msg) {
+  if (!msg) {
+    error.style.display = "none";
+  } else {
+    error.textContent = msg;
+    error.style.display = "block";
+  }
+}
+
 function completeForm() {
   card.innerHTML = `
     <h1>Thanks ${nameInput}!</h1>
-    <p>We'll be spamming ${emailInput}</p>
-    <p>and scamming ${phoneInput} shortly!</p>
+    <p>We'll be spamming <strong>${emailInput}</strong></p>
+    <p>and scamming <strong>${phoneInput}</strong> shortly!</p>
   `;
 }
